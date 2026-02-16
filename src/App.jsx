4452b89bf666fd4232047e7928533582f9366429
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-16 10:36:30 +0100

    milestone-1
    API call made and card created to display the data obtained

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react"
 
 function App() {
+  const urlApi = 'http://localhost:3333/politicians'
+  const [data, setData] = useState([])
 
+  const loadPoliticians = async () => {
+    const res = await fetch(urlApi)
+    const data = await res.json()
+    setData(data)
+  }
+  useEffect(() => {
+    loadPoliticians()
+  }, [])
 
   return (
     <>
-
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              {
+                data.map(el => (
+                  <div className="card" key={el.id}>
+                    <h2>{el.name}</h2>
+                    <img src={el.image} alt="" />
+                    <h3>{el.position}</h3>
+                    <p>{el.biography}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
